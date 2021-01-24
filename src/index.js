f8e67c89709592b6d69175cc753fbd8964b9151b
sandeepm123 <sandeepm956@gmail.com>
2021-01-24 19:56:00 +0530

    login and header Components changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from './App';
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
-    <span>
-        Image Viewer
-    </span>, 
-    document.getElementById('root')
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
 );
